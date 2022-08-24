9fddd2fdec278e136bd79e4f351dbfc2d08409f8
madina <91madina@gmail.com>
2022-08-24 23:04:08 +0600

    module #4, taskFinal

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        int result;
+        for (int i = 1; i <= 10; i++) {
+            result = i * numberTableToPrint;
+            System.out.println(i + " x " + numberTableToPrint + " = " + result);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,9 +3,22 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
 
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength; j++) {
+                if (j == cathetusLength) {
+                    System.out.print(1);
+                } else if (j == cathetusLength - 1 || j == cathetusLength + 1) {
+                    System.out.print(2);
+                }
+
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
 }
+
+
